78917f1f9c718036834157a3ec288420eb5931c1
= <klas_hermansson@hotmail.com>
2023-10-27 14:50:58 +0200

    Splitting string in halv solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] data = File.ReadAllLines("data.txt");
+int sum;
+
+foreach (string line in data)
+{
+    string compartment1 = line.Substring(0, (line.Length / 2));
+    string compartment2 = line.Substring(line.Length / 2, line.Length / 2);
+    for (int i = 0; i < compartment1.Length; i++)
+    {
+        if (compartment1[i].Equals(compartment2[i]))
+        {
+            switch(compartment1[i])
+            {
+
+            }
+
+        }
+    }
+}
