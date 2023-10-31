2f8f0cef65d9f3e26fb9ae8ce9b0bc183396a8f7
= <klas_hermansson@hotmail.com>
2023-10-31 13:09:31 +0100

    Aaalmost theeere

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿string[] data = File.ReadAllLines("data.txt");
-int sum;
+int sum = 0;
 
 foreach (string line in data)
 {
     string compartment1 = line.Substring(0, (line.Length / 2));
     string compartment2 = line.Substring(line.Length / 2, line.Length / 2);
+    Console.WriteLine(compartment1);
+    Console.WriteLine(compartment1);
+    int item = 0;
     for (int i = 0; i < compartment1.Length; i++)
     {
-        if (compartment1[i].Equals(compartment2[i]))
+        if (compartment2.Contains(compartment1[i]))
         {
-            switch(compartment1[i])
+            if ((int) compartment1[i] is > 64 and < 91)
             {
-
+                Console.WriteLine(compartment1[i]);
+                item = ((int)compartment1[i] - 64);
             }
-
+            else if ((int)compartment1[i] is > 96 and < 123)
+            {
+                Console.WriteLine(compartment1[i]);
+                item = ((int)compartment1[i] - 96);
+            }
         }
     }
+    sum += item;
 }
+Console.WriteLine(sum);
