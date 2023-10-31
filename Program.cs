2ee437a285815f3c99463ef0f4d1e9abf98f46ce
= <klas_hermansson@hotmail.com>
2023-10-31 13:48:12 +0100

    Working

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ foreach (string line in data)
 {
     string compartment1 = line.Substring(0, (line.Length / 2));
     string compartment2 = line.Substring(line.Length / 2, line.Length / 2);
-    Console.WriteLine(compartment1);
-    Console.WriteLine(compartment1);
     int item = 0;
     for (int i = 0; i < compartment1.Length; i++)
     {
@@ -14,12 +12,10 @@ foreach (string line in data)
         {
             if ((int) compartment1[i] is > 64 and < 91)
             {
-                Console.WriteLine(compartment1[i]);
-                item = ((int)compartment1[i] - 64);
+                item = ((int)compartment1[i] - 38);
             }
             else if ((int)compartment1[i] is > 96 and < 123)
             {
-                Console.WriteLine(compartment1[i]);
                 item = ((int)compartment1[i] - 96);
             }
         }
